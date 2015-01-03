a8a159b4a4fc85805d237b8745d6a926c19a0e42
kamoorr <rkamoor@gmail.com>
2015-01-02 20:01:00 -0500

    demo

---
+++ demo-sites/duo-theme/blog_files/t.js
@@ -0,0 +1,36 @@
+<HTML><HEAD>
+<TITLE>Access Denied</TITLE>
+</HEAD>
+<BODY>
+<FONT face="Helvetica">
+<big><strong></strong></big><BR>
+</FONT>
+<blockquote>
+<TABLE border=0 cellPadding=1 width="80%">
+<TR><TD>
+<FONT face="Helvetica">
+<big>Access Denied (content_filter_denied)</big>
+<BR>
+<BR>
+</FONT>
+</TD></TR>
+<TR><TD>
+<FONT face="Helvetica">
+Your request was denied because of its content categorization: "File Storage/Sharing"
+</FONT>
+</TD></TR>
+<TR><TD>
+<FONT face="Helvetica">
+
+</FONT>
+</TD></TR>
+<TR><TD>
+<FONT face="Helvetica">
+<BR>
+For assistance, contact your local IT support via Ehelp or contact Division IT Help Desk by calling 1-866-995-4700 or send an email to DS.HelpMeIT@adp.com.
+</FONT>
+</TD></TR>
+</TABLE>
+</blockquote>
+</FONT>
+</BODY></HTML>
